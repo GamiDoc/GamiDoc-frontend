b85fdd5946616a24a22ce2fc583683e41c9005af
bottarocarlo <bottarocarloo@gmail.com>
2022-05-27 10:28:24 +0200

    PDF.js

---
+++ components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import logo from "../public/Logo.png";
+import Image from "next/image";
+
+function Header() {
+  return (
+    <div className=" relative px-24 py-9 flex flex-wrap items-center justify-between mb-3">
+      <Image src="/../public/Logo.png" alt="logo" width={119} height={34} />
+      <div className="flex flex-row gap-5" id="button">
+        <button className="py-4 inline-block px-8 bg-yellow-gamy text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md  hover:bg-yellow-600 hover:shadow-lg focus:bg-yellow-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out">
+          SIGN UP
+        </button>
+        <button className="py-4 inline-block px-8 bg-gray-800 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md  hover:bg-gray-600 hover:shadow-lg focus:bg-gray-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out">
+          SIGN IN
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ pages/form.js
@@ -0,0 +1,169 @@
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+
+import React, { Component } from "react";
+import PDF from "../components/PDF";
+
+class Post extends Component {
+  state = {
+    title: "",
+    content: "",
+    image: "",
+    postSubmitted: false,
+  };
+
+  onChange = (input) => (e) => {
+    this.setState({
+      [input]: e.target.value,
+    });
+  };
+
+  sunmitPost = (e) => {
+    if (!this.state.title || !this.state.content) {
+      alert("All fields are required!");
+      e.preventDefault();
+    } else {
+      this.setState({
+        postSubmitted: true,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="flex flex-col h-screen justify-between">
+        <Header />
+
+        {!this.state.postSubmitted ? (
+          <form>
+            <fieldset>
+              <legend className="text-center header">Add new Post</legend>
+              <div className="form-group">
+                <span className="col-md-1 col-md-offset-2 text-center">
+                  <i className="fa fa-user bigicon"></i>
+                </span>
+                <input
+                  onChange={this.onChange("title")}
+                  name="title"
+                  type="text"
+                  placeholder="Post Title"
+                  className="form-control"
+                />
+              </div>
+              <div className="form-group">
+                <span className="col-md-1 col-md-offset-2 text-center">
+                  <i className="fa fa-user bigicon"></i>
+                </span>
+                <input
+                  onChange={this.onChange("image")}
+                  name="image"
+                  type="text"
+                  placeholder="https://"
+                  className="form-control"
+                />
+              </div>
+              <div className="form-group">
+                <span className="col-md-1 col-md-offset-2 text-center">
+                  <i className="fa fa-pencil-square-o bigicon"></i>
+                </span>
+                <textarea
+                  onChange={this.onChange("content")}
+                  className="form-control"
+                  name="content"
+                  placeholder="Enter your text here"
+                  rows="7"
+                ></textarea>
+              </div>
+              <div className="form-group">
+                <button
+                  type="button"
+                  onClick={this.sunmitPost}
+                  className="btn btn-primary btn-lg"
+                >
+                  Submit
+                </button>
+              </div>
+            </fieldset>
+          </form>
+        ) : (
+          <PDF
+            title={this.state.title}
+            content={this.state.content}
+            image={this.state.image}
+          />
+        )}
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Post;
+
+/*
+ <div className="container">
+            <div className="jumbotron mt-3">
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="well well-sm">
+                    <form className="form-horizontal" method="post">
+                      <fieldset>
+                        <legend className="text-center header">
+                          Add new Post
+                        </legend>
+                        <div className="form-group">
+                          <span className="col-md-1 col-md-offset-2 text-center">
+                            <i className="fa fa-user bigicon"></i>
+                          </span>
+                          <input
+                            onChange={this.onChange("title")}
+                            name="title"
+                            type="text"
+                            placeholder="Post Title"
+                            className="form-control"
+                          />
+                        </div>
+                        <div className="form-group">
+                          <span className="col-md-1 col-md-offset-2 text-center">
+                            <i className="fa fa-user bigicon"></i>
+                          </span>
+                          <input
+                            onChange={this.onChange("image")}
+                            name="image"
+                            type="text"
+                            placeholder="https://"
+                            className="form-control"
+                          />
+                        </div>
+                        <div className="form-group">
+                          <span className="col-md-1 col-md-offset-2 text-center">
+                            <i className="fa fa-pencil-square-o bigicon"></i>
+                          </span>
+                          <textarea
+                            onChange={this.onChange("content")}
+                            className="form-control"
+                            name="content"
+                            placeholder="Enter your text here"
+                            rows="7"
+                          ></textarea>
+                        </div>
+                        <div className="form-group">
+                          <button
+                            type="button"
+                            onClick={this.sunmitPost}
+                            className="btn btn-primary btn-lg"
+                          >
+                            Submit
+                          </button>
+                        </div>
+                      </fieldset>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+*/
