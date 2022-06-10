b3e0480a80f0a3271a80c7356de9ec64e3e5d08b
bottarocarlo <bottarocarloo@gmail.com>
2022-06-10 15:46:02 +0200

    pop-up with name appears

--- pages/form.js
+++ pages/form.js
@@ -6,6 +6,7 @@ import NavbarIcon from "../components/NavbarIcon";
 import dynamic from "next/dynamic";
 const Pdf = dynamic(() => import("../components/CreatePDF"), { ssr: false });
 import { Listbox, Transition } from "@headlessui/react";
+import { Dialog } from "@headlessui/react";
 import { CheckIcon, SelectorIcon } from "@heroicons/react/solid";
 import TextareaAutosize from "@mui/material/TextareaAutosize";
 
@@ -66,27 +67,21 @@ export default function Home() {
   const [aim, setAim] = useState(Aimo[0]);
   const [name, setName] = useState("");
   const [target, setTarget] = useState("");
+  let [isOpen, setIsOpen] = useState(true);
 
+  function closeModal() {
+    setIsOpen(false);
+  }
 
+  function openModal() {
+    setIsOpen(true);
+  }
 
   function handleSwitch() {
     switch (selected) {
       case 1:
         return (
           <div className="flex flex-col py-4">
-             <label className="block text-gray-700 text-sm font-bold mb-2">
-              Name of the project
-            </label>
-            <TextareaAutosize
-              className="w-96 border cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none   sm:text-sm"
-              id="Name"
-              type="text"
-              minRows={1}
-              placeholder="Name of the project"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-
             <label className=" mt-4 block text-gray-700 text-sm font-bold mb-2">
               Domain
             </label>
@@ -279,7 +274,13 @@ export default function Home() {
           </div>
 
           <div className="flex flex-row justify-end gap-5 mb-2 mr-2">
-            <Pdf name={name} behavior={behavior} domain={domain.name} aim={aim.name} target={target} />
+            <Pdf
+              name={name}
+              behavior={behavior}
+              domain={domain.name}
+              aim={aim.name}
+              target={target}
+            />
             <div
               className="py-4 inline-block px-8 bg-yellow-gamy text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md  hover:bg-yellow-600 hover:shadow-lg focus:bg-yellow-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-400 active:shadow-lg transition duration-150 ease-in-out "
               onClick={() => {
@@ -301,8 +302,65 @@ export default function Home() {
           </div>
         </div>
       </div>
-
       <Footer />
+
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Project name
+                  </Dialog.Title>
+                  <div className="mt-2">
+                    <TextareaAutosize
+                      className="w-96 border cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none   sm:text-sm"
+                      id="Name"
+                      type="text"
+                      minRows={1}
+                      placeholder="Name of the project"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </div>
+                  <div className="mt-4">
+                    <button
+                      type="button"
+                      className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                      onClick={closeModal}
+                    >
+                      Save!
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
     </div>
   );
 }
