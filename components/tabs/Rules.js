af786ac8249977447188aa3aea06e7216910223b
carlo bottaro <bottarocarloo@gmail.com>
2022-08-08 09:49:13 +0200

    Added affordances in Rules tab

--- components/tabs/Rules.js
+++ components/tabs/Rules.js
@@ -5,19 +5,29 @@ import IconButton from "@mui/material/IconButton";
 import { useRouter } from "next/router";
 import Link from "next/link";
 
-function Rules({ rules, setRules }) {
+function Rules({
+  rules,
+  setRules,
+  affordances1,
+  affordances2,
+  affordances3,
+  affordances4,
+  affordances5,
+  affordances6,
+}) {
   const router = useRouter();
 
   return (
     <div className="flex flex-col py-4 w-[60em]">
       <label className="mt-4 block text-gray-700  font-bold mb-2">
         <Link href="/documentation#gamerules">
-        <IconButton
-          aria-label="Example"
-          //onClick={() => router.push("/documentation#gamerules")}
-        >
-          <InfoOutlinedIcon sx={{ fontSize: 20 }} />
-        </IconButton></Link>
+          <IconButton
+            aria-label="Example"
+            //onClick={() => router.push("/documentation#gamerules")}
+          >
+            <InfoOutlinedIcon sx={{ fontSize: 20 }} />
+          </IconButton>
+        </Link>
         Rules
       </label>
       <h2 className="w-[60em] mt-4 block text-gray-700  mb-2 ">
@@ -34,6 +44,87 @@ function Rules({ rules, setRules }) {
         placeholder="Rules"
         value={rules}
         onChange={(e) => setRules(e.target.value)}
+      />
+
+
+
+      <TextField
+        className={
+          affordances1 == ""
+            ? "hidden w-full border  shadow-md "
+            : "flex w-full border  shadow-md mb-4"
+        }
+        type="text"
+        multiline
+        rows={3}
+        placeholder="Rules for ${affordances1}"
+        // value={behavior}
+        // onChange={(e) => setBehavior(e.target.value)}
+      />
+      <TextField
+        className={
+          affordances2 == ""
+            ? "hidden w-full border  shadow-md "
+            : "flex w-full border  shadow-md mb-4"
+        }
+        type="text"
+        multiline
+        rows={3}
+        placeholder={affordances4}
+        // value={behavior}
+        // onChange={(e) => setBehavior(e.target.value)}
+      />
+      <TextField
+        className={
+          affordances3 == ""
+            ? "hidden w-full border  shadow-md "
+            : "flex w-full border  shadow-md mb-4"
+        }
+        type="text"
+        multiline
+        rows={3}
+        placeholder={affordances4}
+        // value={behavior}
+        // onChange={(e) => setBehavior(e.target.value)}
+      />
+      <TextField
+        className={
+          affordances4 == ""
+            ? "hidden w-full border  shadow-md "
+            : "flex w-full border  shadow-md mb-4"
+        }
+        type="text"
+        multiline
+        rows={3}
+        placeholder={affordances4}
+        // value={behavior}
+        // onChange={(e) => setBehavior(e.target.value)}
+      />
+      <TextField
+        className={
+          affordances5 == ""
+            ? "hidden w-full border  shadow-md "
+            : "flex w-full border  shadow-md mb-4"
+        }
+        type="text"
+        multiline
+        rows={3}
+        placeholder={affordances4}
+        // value={behavior}
+        // onChange={(e) => setBehavior(e.target.value)}
+      />
+      <TextField
+        className={
+          affordances6 == ""
+            ? "hidden w-full border  shadow-md "
+            : "flex w-full border  shadow-md mb-4"
+        }
+        type="text"
+        multiline
+        rows={3}
+        placeholder={affordances4}
+        // value={behavior}
+        // onChange={(e) => setBehavior(e.target.value)}
       />
     </div>
   );
