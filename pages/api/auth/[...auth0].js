9157c7f3693c01a3082c79df4c546565185739dd
Andrea Mangrella <mangrella.andrea@gmail.com>
2022-08-18 15:24:30 +0200

    auth

---
+++ pages/api/auth/[...auth0].js
@@ -0,0 +1,31 @@
+require('dotenv').config()
+import { handleAuth, handleLogin, handleCallback } from '@auth0/nextjs-auth0';
+
+const afterCallback = async (req, res, session, state) => {
+  console.log(session);
+  return session;
+}
+
+export default handleAuth({
+  async login(req, res) {
+    try {
+      await handleLogin(req, res, {
+        authorizationParams: {
+          audience: (process.env.SECURE ? 'https://' : 'http://') + process.env.BACK_ENDPOINT,
+          scope: 'openid profile email read:messages'
+        },
+        returnTo: "/models/routes/checkProfile"
+        // da controllare 
+      });
+    } catch (error) {
+      res.status(error.status || 400).end(error.message);
+    }
+  },
+  async callback(req, res) {
+    try {
+      await handleCallback(req, res, { afterCallback });
+    } catch (error) {
+      res.status(error.status || 500).end(error.message);
+    }
+  }
+});
